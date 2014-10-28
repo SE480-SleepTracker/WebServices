8ba7bf3bec043934602ca341ef31029a2929602d
ethansudman@comcast.net <ethansudman@comcast.net>
2014-10-27 21:15:57 -0500

    Update several packages

---
+++ SE450 Sleep Tracker Web API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SE450_Sleep_Tracker_Web_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SE450 Sleep Tracker Web API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SE450_Sleep_Tracker_Web_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
